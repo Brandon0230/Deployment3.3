534a17efa331784e5d26ad11b61e1176cadbac26
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2025-09-14 12:12:39 +1000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,20 @@
+<?php 
+$serverCurrentTime = time();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>About</title>
+<link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+    <nav>
+    <a href="index.php">Home</a>
+    <a href="about.php">About</a>
+    </nav>
+<h1>About App</h1>
+<p>Created by Brandon Peters 103604188</p>
+<h3>PHP is a good language for Data handling especially with SQL.</h3>
+</body>
+</html>
